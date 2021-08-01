dcdf306009c47895ee0020747c8200f70e773e46
miharatoki <kou566533@gmail.com>
2021-08-01 09:31:45 +0000

    投稿一覧画面をいいね数の多い順にランキング表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,15 @@
 class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :favorites, source: :user
 	has_many :book_comments, dependent: :destroy
 	belongs_to :user
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
+	end
+	
+	def self.favorit_last_week
+	  Book.joins(:favorites).where(favorites: {created_at: 0.days.ago.prev_week..0.days.ago.prev_week(:sunday)}).group(:id).order("count(*) desc")
 	end
   
   def self.looks(searchs, words)
